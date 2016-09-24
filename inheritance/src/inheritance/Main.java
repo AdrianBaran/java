8c7160fbc93c229d79b5bebdf439448e9ccd075c
Adrian Baran <adrianbaran@gmail.com>
2016-09-24 15:38:15 +0100

    Fun with math and class

--- inheritance/src/inheritance/Main.java
+++ inheritance/src/inheritance/Main.java
@@ -3,7 +3,22 @@ package inheritance;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("My first push of java");
+
+		int sum = MathUtil.addNumbersStatic(7, new Integer(8));
+		System.out.println("sum:" + sum);
+		
+		MathUtil util = new MathUtil();
+		MathUtil util2=util;
+		
+		int addNumbers = util.addNumbers(0, -2);
+		
+		
+		
+		System.out.println("sum2" + new MathUtil().addNumbers(9, 9));
+		
+		
+		
+
 	}
-	
+
 }
---
+++ inheritance/src/inheritance/MathUtil.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class MathUtil {
+
+	public int addNumbers(int a, int b) {
+		return a + b;
+	}
+
+	public static int addNumbersStatic(int a, int b) {
+		return a + b;
+
+	}
+
+}
