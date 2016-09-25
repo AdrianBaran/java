730568a91a6d0ecf9d0336e37b31860da8716838
Adrian Baran <adrianbaran@gmail.com>
2016-09-26 00:13:32 +0100

    listy poczatek

--- inheritance/src/inheritance/Main.java
+++ inheritance/src/inheritance/Main.java
@@ -1,19 +1,40 @@
 package inheritance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		Person pereson = new Person("Adrian").withLastName("Baran");
 		pereson.setSalary(0L);
-		System.out.println(pereson);
+		
+		Person person2 = new Person("Lukasz","Jackowski", 2700L);
 
+		List<Person> personList = new ArrayList<>();
+		personList.add(person2);
+		personList.add(pereson);
+
+		for (int i = 0; i < personList.size(); i++) {
+			System.out.println(personList.get(i).getFirstName());
+
+		}
 		
-		Manager manager = new Manager("Lidia","Kaleta", 1000L, 250L);
-		System.out.println(manager.toString());
-		
-		HasName manager2 = manager;
-		System.out.println(manager2.getName());
+		for (Person p : personList) {
+			System.out.println(p.getLastName());
+		}
+
+	//	System.out.println(pereson);
+
+		//
+		// Manager manager = new Manager("Lidia","Kaleta", 1000L, 250L);
+		// System.out.println(manager.toString());
+		//
+		// HasName manager2 = manager;
+		// System.out.println(manager2.getName());
+		//
+
 	}
 
 }
